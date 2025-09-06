2bf934b6b190e83a75570ba88d65e99b89de72b6
Renann135 <a24310314@live.ceti.mx>
2025-09-06 01:00:39 +0000

    practica 1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
-#include<iostream>
+#include <Foco.hpp>
 using namespace std;
+//---------------------------------------------------
 int main(int argc, char const *argv[])
 {
-    cout<<"Hola mundo"<<endl;
+    Foco foquito;
+    foquito.Encender();
+    foquito.Apagar();
+
     return 0;
 }
