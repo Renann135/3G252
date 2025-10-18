4fd36ae967150e9dab649071c1de5e4e4000185c
Renann135 <a24310314@live.ceti.mx>
2025-10-18 01:36:36 +0000

    Practica 4

--- src/ejemplo.cpp
+++ src/ejemplo.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <thread>
+#include <chrono>
 using namespace std;
+
 #include <ftxui/screen/screen.hpp>
 #include <ftxui/screen/color.hpp>
 #include <ftxui/component/component.hpp>
+
 #include <ftxui/component/screen_interactive.hpp>
 using namespace ftxui;
+
+string tortuga =
+    string("                __  \n") +
+    string("     .,-;-;-,. /'_| \n") +
+    string("   _/_/_/_|_|_|) /  \n") +
+    string(" '-<_><_><_><_>=/|  \n") +
+    string("   `/_/====/_/-'|_| \n") +
+    string("    "
+           "     "
+           "    "
+           " \n");
 
 int main(int argc, char const *argv[])
 {
@@ -15,11 +32,38 @@ int main(int argc, char const *argv[])
 
     int x = 0;
     int y = 0;
-    auto &pixel = pantalla.PixelAt(1, 1);
-    pixel.blink = true;
-    pixel.background_color = Color::Blue;
-    pixel.foreground_color = Color::Red;
-    pixel.character = 'A';
-    cout << pantalla.ToString() << endl;
+
+    while (true)
+    {
+        x++;
+        stringstream ss(tortuga);
+        string linea;
+        int nolinea = 0;
+        while (getline(ss, linea))
+        {
+            int columna = 0;
+            for (auto &&letra : linea)
+            {
+                auto &pixel = pantalla.PixelAt(x + columna, y + nolinea);
+                pixel.character = letra;
+                columna++;
+            }
+            nolinea++;
+        }
+
+        // auto &pixel = pantalla.PixelAt(x, y);
+        // pixel.blink = true;
+        // pixel.background_color = Color::Blue;
+        // pixel.foreground_color = Color::Red;
+        // pixel.character = tortuga;
+        // cout << pantalla.ToString() << endl;
+
+        pantalla.Print();
+        pantalla.Clear();
+        cout << pantalla.ResetPosition();
+        this_thread::sleep_for(chrono::milliseconds(500));
+        cout << pantalla.ResetPosition();
+    }
+
     return 0;
 }
